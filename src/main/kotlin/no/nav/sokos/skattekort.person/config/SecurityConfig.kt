b3f926d84cb0f812cf0a27adfccbc0f5e9728e9c
Tak Wai Wang <tak.wai.wang@nav.no>
2022-12-12 14:28:07 +0100

    test med proxy i jwkProvider

--- src/main/kotlin/no/nav/sokos/skattekort.person/config/SecurityConfig.kt
+++ src/main/kotlin/no/nav/sokos/skattekort.person/config/SecurityConfig.kt
@@ -2,6 +2,9 @@ package no.nav.sokos.skattekort.person.config
 
 import com.auth0.jwk.JwkProvider
 import com.auth0.jwk.JwkProviderBuilder
+import io.ktor.client.engine.ProxyBuilder
+import io.ktor.client.engine.http
+import io.ktor.network.sockets.InetSocketAddress
 import io.ktor.server.application.Application
 import io.ktor.server.auth.authentication
 import io.ktor.server.auth.jwt.JWTPrincipal
@@ -12,6 +15,7 @@ import mu.KotlinLogging
 import no.nav.sokos.skattekort.person.config.Config.AzureAdConfig
 import no.nav.sokos.skattekort.person.config.Config.OpenIdMetadata
 import no.nav.sokos.skattekort.person.config.Config.wellKnowConfig
+import java.net.Proxy
 
 private val log = KotlinLogging.logger {}
 const val AUTHENTICATION_NAME = "azureAd"
@@ -61,7 +65,17 @@ fun Application.configureSecurity(
 }
 
 private fun cachedJwkProvider(jwksUri: String): JwkProvider {
-    return JwkProviderBuilder(URL(jwksUri))
+    return System.getenv("HTTP_PROXY")?.let {
+        JwkProviderBuilder(URL(jwksUri))
+            .proxied(ProxyBuilder.http(it))
+            .cached(10, 24, TimeUnit.HOURS) // cache up to 10 JWKs for 24 hours
+            .rateLimited(
+                10,
+                1,
+                TimeUnit.MINUTES
+            ) // if not cached, only allow max 10 different keys per minute to be fetched from external provider
+            .build()
+    } ?: JwkProviderBuilder(URL(jwksUri))
         .cached(10, 24, TimeUnit.HOURS) // cache up to 10 JWKs for 24 hours
         .rateLimited(
             10,
