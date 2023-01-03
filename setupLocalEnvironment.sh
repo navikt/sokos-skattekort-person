d23ef5bc7265377df626c93bf23180d30769a240
mudasar187 <mudasar_cheema@hotmail.com>
2023-01-03 13:20:35 +0100

    lar init-script håndtere innhenting av username for database

--- setupLocalEnvironment.sh
+++ setupLocalEnvironment.sh
@@ -9,7 +9,7 @@ kubectl config use-context dev-fss
 kubectl config set-context --current --namespace=okonomi
 
 # Get AZURE and DATABASE system variables
-envValue=$(kubectl exec -it $(kubectl get pods | grep sokos-skattekort-person | cut -f1 -d' ') -c sokos-skattekort-person -- env | egrep "AZURE")
+envValue=$(kubectl exec -it $(kubectl get pods | grep sokos-skattekort-person | cut -f1 -d' ') -c sokos-skattekort-person -- env | egrep "AZURE|DATABASE")
 
 # Set AZURE as local environment variables
 rm -f defaults.properties
