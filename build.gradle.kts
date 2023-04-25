74112bac6ccf545e09a1be35f7ed0d7e5b9fa0b7
mudasar187 <mudasar_cheema@hotmail.com>
2023-04-25 10:47:06 +0200

    bumper opp dependencies versjoner for jvm og serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.20"
-    kotlin("plugin.serialization") version "1.8.20"
+    kotlin("jvm") version "1.8.21"
+    kotlin("plugin.serialization") version "1.8.21"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 
     application
