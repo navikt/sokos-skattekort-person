4b45277cb566ec9efb4434a0c6065a35a9c013a7
mudasar187 <mudasar_cheema@hotmail.com>
2023-04-26 09:10:58 +0200

    fjerner inntektsaar under skattekort objektet fordi v2 og v3 ikke har det feltet lenger

--- src/main/kotlin/no/nav/sokos/skattekort.person/domain/Skattekort.kt
+++ src/main/kotlin/no/nav/sokos/skattekort.person/domain/Skattekort.kt
@@ -3,7 +3,6 @@ package no.nav.sokos.skattekort.person.domain
 import java.time.LocalDate
 
 data class Skattekort(
-    val inntektsaar: Long,
     val utstedtDato: LocalDate,
     val skattekortidentifikator: Long,
     val forskuddstrekk: List<Forskuddstrekk>? = null
