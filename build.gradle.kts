b5b01723b0cf89186ab8ca2efb33f0c861e4f631
mudasar187 <mudasar_cheema@hotmail.com>
2025-08-11 13:02:43 +0200

    bumper opp gradle til 9.0.0 og oppdaterer wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "2.2.0"
-    id("com.gradleup.shadow") version "9.0.1"
+    id("com.gradleup.shadow") version "9.0.0"
     id("com.expediagroup.graphql") version "8.8.1"
     id("org.jlleitschuh.gradle.ktlint") version "13.0.0"
     id("org.jetbrains.kotlinx.kover") version "0.9.1"
@@ -173,7 +173,7 @@ tasks {
     }
 
     withType<Wrapper> {
-        gradleVersion = "8.14"
+        gradleVersion = "9.0.0"
     }
 
     withType<GraphQLGenerateClientTask>().configureEach {
